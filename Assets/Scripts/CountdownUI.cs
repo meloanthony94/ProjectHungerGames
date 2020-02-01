5d2afb0d48be4f63771cf062a126e7ad15149020
Yu-Jen Lin <yjlin5210@gmail.com>
2020-02-01 18:11:40 -0500

    feature(ui): countdown timer

---
+++ Assets/Scripts/CountdownUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownUI : MonoBehaviour
+{
+
+    public Sprite[] imageSprites;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        image.sprite = imageSprites[2];
+    }
+
+    // Update is called once per frame
+    public void UpdateValue(float value)
+    {
+        if (value > 2)
+        {
+            image.sprite = imageSprites[2];
+        }
+        else if (value > 1)
+        {
+            image.sprite = imageSprites[1];
+        }
+        else
+        {
+            image.sprite = imageSprites[0];
+        }
+    }
+}
