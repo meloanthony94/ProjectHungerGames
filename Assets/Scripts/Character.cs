ff13a66999d06ec64808b1c67978011762cb3b85
Meloanthony94 <anthony.melo@universalorlando.com>
2020-02-01 04:44:08 -0500

    config(Set Up): Set up folders and Plot/Character classes

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum ActionType
+{
+    Dig,
+    Plant,
+    Harvest,
+    Eat
+}
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent EatEvent;
+
+    Plot CurrentPlotReference = null;
+
+    [Header("Character Variables")]
+    [SerializeField]
+    bool isCritter = false;
+    [SerializeField]
+    int movementSpeed;
+    [SerializeField]
+    int dashSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+
+        //Raycast
+        //if hit a plot, grab that ref if current ref is null
+        //if not, unhighlight, set to null or to new ref
+        //Ternary
+        //check against PlotLayer
+    }
+}
---
+++ Assets/Scripts/Plot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plot : MonoBehaviour
+{
+    private bool isOccupied = false;
+
+    private int highlightCounter = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool Action(ActionType _action)
+    {
+        return false;
+    }
+
+    public void Highlight(bool isOn)
+    {
+
+    }
+
+}
