85b1c6661b1c76af337a7a49facc496a5218d85c
Meloanthony94 <anthony.melo@universalorlando.com>
2020-02-01 11:44:33 -0500

    feat(Character Movement): The character can be moved via the analog stick. In addition, action and dash have been mapped to X and O

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -26,9 +26,25 @@ public class Character : MonoBehaviour
     bool isCritter = false;
     [SerializeField]
     [Range(0.0f, 1.0f)]
-    float movementSpeed;
+    float walkSpeed = 0.1f;
+
+    float currentMovementSpeed = 0.1f;
+
+    [Header("Dash Variables")]
     [SerializeField]
-    int dashSpeed;
+    [Range(0.0f, 2000.0f)]
+    float dashForce;
+    [SerializeField]
+    ForceMode DashType;
+
+    bool isDashing = false;
+    bool isDashingLocked = false;
+    [Range(0.0f, 10.0f)]
+    public float dashCooldownTime = 3;
+    [Range(0.0f, 5.0f)]
+    //The variable controls when the user can start moving again during dash. 0 = very end of dash
+    public float minimumDashVelocity = 0.5f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -40,18 +56,45 @@ public class Character : MonoBehaviour
     void Update()
     {
         //Movement
-        Vector3 newPos = transform.position + ((Vector3.right  * Input.GetAxis("Horizontal")) * movementSpeed);
-        newPos = newPos + ((Vector3.forward * Input.GetAxis("Vertical")) * movementSpeed);
-        myRigidBody.MovePosition(newPos); 
+        if (!isDashing)
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            if (input != Vector3.zero)
+            {
+                transform.forward = input;
+                myRigidBody.MovePosition(transform.position + (transform.forward * walkSpeed));
+                //transform.Translate(transform.forward * movementSpeed, Space.World);
+            }
+        }
+        else
+        {
+            if (myRigidBody.velocity.magnitude < minimumDashVelocity)
+            {
+                isDashing = false;
+                StartCoroutine(DashCooldownTimer());
+            }
+        }
         //
-        
-        if (Input.GetButtonDown("Action"))
-            print("action");
+
+        if (Input.GetButtonDown("Dash") && !isDashing && !isDashingLocked)
+        {
+            isDashing = true;
+            myRigidBody.AddForce(transform.forward * dashForce, DashType);
+        }
 
         //Raycast
         //if hit a plot, grab that ref if current ref is null
         //if not, unhighlight, set to null or to new ref
         //Ternary
         //check against PlotLayer
+    }
+
+    IEnumerator DashCooldownTimer()
+    {
+        isDashingLocked = true;
+
+        yield return new WaitForSeconds(dashCooldownTime);
+
+        isDashingLocked = false;
     }
 }
