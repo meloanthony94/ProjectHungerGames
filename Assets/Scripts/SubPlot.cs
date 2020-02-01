3d8c0c02c4000f75ad6f9f954ccf24059974e13a
Yu-Jen Lin <yjlin5210@gmail.com>
2020-02-01 10:43:43 -0500

    feature(plot): basic plot behavior implemented

---
+++ Assets/Scripts/SubPlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubPlot : MonoBehaviour
+{
+    [SerializeField]
+    private Material defaultMaterial;
+
+    [SerializeField]
+    private Material highlightMaterial;
+
+    private Renderer _renderer;
+
+    public void Initialize()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void SetHighlight(bool isOn)
+    {
+        if(isOn)
+        {
+            _renderer.material = highlightMaterial;
+        }
+        else
+        {
+            _renderer.material = defaultMaterial;
+        }
+    }
+}
